be39927235c2efb64d3e823ab0eb7c5e8b7457c5
gaetano-94 <gaetano.panico1994@gmail.com>
2024-05-21 15:53:40 +0200

    feat: stampare in html la risposta del server

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 const http = require('http');
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || 'localhost';
+// const mex = process.env.MEX || 'Gaetano Panico';
+const mex = process.env.MEX || 'Nessun Messaggio';
 http
   .createServer(function (req, res) {
     res.writeHead(200, {
       'Content-Type': 'text/html',
     });
-    res.end('Bella genteee');
+    res.end(`<h1>${mex}</h1>`);
   })
   .listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`;
