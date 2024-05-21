f8170d89fe512e5aeeb8a77c8315c0064b1ff871
gaetano-94 <gaetano.panico1994@gmail.com>
2024-05-21 18:14:32 +0200

    feat: frasi motivazionali random

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const http = require('http');
+const { getRandomQuote } = require('./frasimotivazionali');
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || 'localhost';
 const mex = process.env.MEX || 'Nessun Messaggio';
@@ -8,7 +9,18 @@ http
     res.writeHead(200, {
       'Content-Type': 'text/html',
     });
-    res.end(`<h1>${mex}</h1>`);
+    res.end(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bella Gente</title>
+    </head>
+    <body>
+        <p>${mex}</p>
+        <h1>${getRandomQuote()}</h1>
+    </body>
+    </html>`);
   })
   .listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`;
